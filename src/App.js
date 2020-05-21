07095adae2891250abccdfd767844afda29cb671
qiannyqian <liang.huiqian@gmail.com>
2020-05-21 12:25:05 +0800

    changed readme, updated app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiEndpoint = 'https://api.randomuser.me/';
+
+class App extends Component {
+  state = {
+    loading: true,
+    person: null,
+  };
+
+  async componentDidMount() {
+    console.log('Component mounted');
+    const url = apiEndpoint;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    this.setState({ person: data.results[0], loading: false });
+  }
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>No person found.</div>;
+    }
+
+    return (
+      <div className="App">
+        <div> {this.state.person.name.title}</div>
+        <div> {this.state.person.name.first}</div>
+        <div> {this.state.person.name.last}</div>
+        <img src={this.state.person.picture.large} alt="Person" />
+      </div>
+    );
+  }
 }
 
 export default App;
